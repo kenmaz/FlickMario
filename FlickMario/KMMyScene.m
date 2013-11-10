9a4903bff81793ba84d032791bd81ebb4b6ee14e
kenmaz <kenmaz@kenmaz-no-iMac.local>
2013-11-11 00:55:03 +0900

    とりあえず降りていく版の原型

--- FlickMario/KMMyScene.m
+++ FlickMario/KMMyScene.m
@@ -8,46 +8,88 @@
 
 #import "KMMyScene.h"
 
+@interface KMMyScene()
+@property BOOL contentsCreated;
+@property SKSpriteNode* mario;
+@property CGPoint touchStartPt;
+@end
+
 @implementation KMMyScene
 
--(id)initWithSize:(CGSize)size {    
-    if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
-        
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
-        
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
+- (void)didMoveToView:(SKView *)view {
+    if (!self.contentsCreated) {
+        [self createSceneContents];
+        self.contentsCreated = YES;
     }
-    return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+- (void)createSceneContents {
+    self.backgroundColor = [SKColor colorWithRed:0.0 green:0.5 blue:1.0 alpha:1.0];
+    self.scaleMode = SKSceneScaleModeAspectFit;
+    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
+    SKSpriteNode* ground = [[SKSpriteNode alloc] initWithColor:[SKColor brownColor] size:CGSizeMake(self.size.width, 50)];
+    ground.position = CGPointMake(CGRectGetMidX(self.frame), 25);
+    ground.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ground.size];
+    ground.physicsBody.dynamic = NO;
+    [self addChild:ground];
+
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(70, 100)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(250, 150)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(300, 200)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(80, 250)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(240, 300)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(50 , 350)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(100, 400)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(200, 450)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(300, 500)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(80, 550)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(240, 600)];
+    [self addPlateWithSize:CGSizeMake(100, 10) point:CGPointMake(50 , 650)];
+    
+    SKSpriteNode* mario = [[SKSpriteNode alloc] initWithColor:[SKColor redColor] size:CGSizeMake(15, 30)];
+    mario.name = @"mario";
+    mario.position = CGPointMake(self.size.width - 50, self.size.height - 50);
+    mario.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:mario.size];
+    mario.physicsBody.dynamic = YES;
+    mario.physicsBody.allowsRotation = NO;
+    mario.physicsBody.mass = 60;
+    [self addChild:mario];
+    self.mario = mario;
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+- (void)addPlateWithSize:(CGSize)size point:(CGPoint)point {
+    SKSpriteNode* block = [[SKSpriteNode alloc] initWithColor:[SKColor brownColor] size:size];
+    block.position = point;
+    block.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:block.size];
+    block.physicsBody.dynamic = NO;
+    [self addChild:block];
+}
+
+static const float touchSensitivity = 200.0;
+static const float adjust = 50;
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch* touch = [touches anyObject];
+    CGPoint pt = [touch locationInNode:self];
+    self.touchStartPt = CGPointMake(pt.x - adjust, pt.y);
+    //NSLog(@"touch start:%@", NSStringFromCGPoint(self.touchStartPt));
+}
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+}
+
+//simulator + magic tracpadだとtouchesEndedの呼び出しにdelayがある
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch* touch = [touches anyObject];
+    CGPoint pt = [touch locationInNode:self];
+    pt = CGPointMake(pt.x - adjust, pt.y);
+    //NSLog(@"touch end:%@", NSStringFromCGPoint(pt));
+ 
+    CGVector impulse = CGVectorMake((pt.x - self.touchStartPt.x) * touchSensitivity,
+                                    (pt.y - self.touchStartPt.y) * touchSensitivity);
+    NSLog(@"impulse:%f,%f", impulse.dx, impulse.dy);
+
+    [self.mario.physicsBody applyImpulse:impulse];
 }
 
 @end
